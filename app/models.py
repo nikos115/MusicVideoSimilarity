8b8a22b3a7e8e301869b2793000fc80fb5eb4aa5
nikos11 <nikos.115@hotmail.com>
2020-01-13 19:41:45 +0200

    change ui

--- app/models.py
+++ app/models.py
@@ -5,19 +5,30 @@ class Feature(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(40), nullable=False)
     type = db.Column(db.String(1), nullable=False)
+    weight = db.Column(db.Float(), nullable=False)
 
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(160), nullable=False)
-    search = db.Column(db.Boolean, nullable=False)
+    search = db.Column(db.Boolean, nullable=False, index=True)
+    directory = db.Column(db.String(250))
     file = db.Column(db.String(250))
+    youtube_id = db.Column(db.String(64))
 
     segments = db.relationship('Segment', lazy='select', backref=db.backref('video', lazy='joined'))
-    meta = db.relationship('Metadata', lazy='select', backref=db.backref('video', lazy='joined'))
+    meta = db.relationship('Meta', lazy='select', backref=db.backref('video', lazy='joined'))
 
-    def youtube_id(self):
-        return self.url.replace('https://www.youtube.com/watch?v=', '')
+    @staticmethod
+    def get_youtube_id(url):
+        return url.replace('http://', '').replace('https://', '').replace('www.youtube.com/watch?v=', '').replace('youtu.be/', '')
+
+    def __init__(self, **kwargs):
+        super(Video, self).__init__(**kwargs)
+        self.youtube_id = Video.get_youtube_id(self.url)
+
+    def __repr__(self):
+        return '<Video id:{} youtube_id:{}, search:{}>'.format(self.id, self.youtube_id, self.search)
 
 
 class Segment(db.Model):
@@ -32,12 +43,13 @@ class Segment(db.Model):
 class SegmentFeatures(db.Model):
     segment_id = db.Column(db.Integer, db.ForeignKey('segment.id'), primary_key=True)
     feature_id = db.Column(db.Integer, db.ForeignKey('feature.id'), primary_key=True)
+    seq_no = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float)
 
     feature = db.relationship('Feature', lazy='joined')
 
 
-class Metadata(db.Model):
+class Meta(db.Model):
     video_id = db.Column(db.Integer, db.ForeignKey('video.id'), primary_key=True)
     title = db.Column(db.String(160), nullable=False)
     artist = db.Column(db.String(160), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
+import threading
 from flask import render_template, flash, redirect, url_for, request, g, jsonify, current_app
 from sqlalchemy import text
 from app import app
 from app import db
-from app.forms import SearchForm
-from app.models import Video
+from app.forms import SearchForm, WeightForm
+from app.models import Video, Meta
+from pytube import YouTube
+from processor import Processor
+import os
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -11,16 +16,30 @@ def index():
     form = SearchForm()
     if form.validate_on_submit():
         video_url = form.q.data
+
         # first check if the video is allready in database
-        video = Video.query.filter_by(url=video_url, search=True).first()
+        video = Video.query.filter_by(youtube_id=Video.get_youtube_id(video_url), search=True).first()
         if video is None:
-            video = Video(url=video_url, search=True)
-            db.session.add(video)
-            db.session.commit()
+            try:
+                yt = YouTube(video_url).streams.filter(progressive=True).last()
+                yt.download()
 
-            # process video, extract features
+            except:
+                flash('Cannot download video. Please choose another.')
 
-        return redirect(url_for('search', video_id=video.id))
+            else:
+                video = Video(url=video_url, search=True)
+                db.session.add(video)
+                db.session.commit()
+
+                # process video, extract features
+                # threading.current_thread().setName('MainThread')
+                p = Processor()
+                p.process_file(yt.default_filename, video.id)
+                os.remove(yt.default_filename)
+
+        if video is not None:
+            return redirect(url_for('search', video_id=video.id))
 
     searches = Video.query.filter_by(search=True).order_by(Video.id.desc()).limit(4).all()
 
@@ -34,8 +53,8 @@ def search(video_id):
     # search
     sql = """
     SELECT haystack.id, haystack.url,
-    (|/ (SUM (haystacksegmentfeatures.value - needlesegmentfeatures.value) ^ 2)) as l2_distance,
-    ((SUM(haystacksegmentfeatures.value * needlesegmentfeatures.value)) / ((|/ SUM(haystacksegmentfeatures.value) ^ 2) * (|/ SUM(needlesegmentfeatures.value) ^ 2))) as cosine_distance
+    (|/ (SUM ((haystacksegmentfeatures.value - needlesegmentfeatures.value) * weight) ^ 2)) as l2_distance,
+    ((SUM(haystacksegmentfeatures.value * needlesegmentfeatures.value) * weight) / ((|/ SUM(haystacksegmentfeatures.value * weight) ^ 2) * (|/ SUM(needlesegmentfeatures.value * weight) ^ 2))) as cosine_distance
     
     FROM video haystack
     INNER JOIN segment haystacksegment ON haystack.id = haystacksegment.video_id
@@ -45,20 +64,43 @@ def search(video_id):
     INNER JOIN segment needlesegment ON needlesegmentfeatures.segment_id = needlesegment.id
     INNER JOIN video needle ON needlesegment.video_id = needle.id AND needle.id != haystack.id AND needle.id = :id
     
+    INNER JOIN feature ON haystacksegmentfeatures.feature_id = feature.id
+    
     where haystack.search = :search
     
-    GROUP BY haystack.id, haystack.url
+    GROUP BY haystack.id, haystack.url, weight
     
-    ORDER BY l2_distance
+    ORDER BY cosine_distance
+    
+    LIMIT 2
     """
 
     # results = db.session.execute(text(sql), id=video_id, search=False)
 
     results = db.session.query(Video).from_statement(text(sql)).params(id=video_id, search=False).all()
+    print(results)
 
-    return render_template('search.html', title='Search Results', video=video, results=results)
+    searches = db.session.query(Video).from_statement(text(sql)).params(id=video_id, search=True).all()
+
+    return render_template('search.html', title='Search Results', video=video, results=results, searches=searches)
 
 
 @app.route('/weights', methods=['GET', 'POST'])
 def weights():
-    pass
+    form = WeightForm()
+    return render_template('weights.html', title='Weights', form=form)
+
+
+@app.route('/genres', methods=['GET'])
+def genres():
+    genres = []
+    for md in Meta.query.distinct(Meta.genre):
+        genres.append(md.genre)
+    return render_template('genres.html', title="Genres", genres=genres)
+
+
+@app.route('/genre/<genre>')
+def genre(genre):
+    videos = db.session.query(Video).join(Meta).filter(Meta.genre == genre)
+    return render_template('genre.html', title='Genre', videos=videos, genre=genre)
+
